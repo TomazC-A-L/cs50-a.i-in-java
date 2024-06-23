8d83c5bac445fddcbe2c8f9a6f61cec42a8056ca
TomazC-A-L <tomazcorrea47@gmail.com>
2024-06-22 21:17:01 -0300

    Comitt for maze

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Maze maze = new Maze("maze1.txt");
+        
     }
 }
--- src/Maze.java
+++ src/Maze.java
@@ -1,6 +1,11 @@
 import java.io.*;
 public class Maze {
     private String filename;
+    private int height;
+    private int width;
+    private Node start;
+    private Node goal;
+
 
     public Maze(String filename) throws IOException{
         this.filename = filename;
@@ -10,11 +15,41 @@ public class Maze {
 
         String linha;
         
+        //Read file and set height and width of maze
         while ((linha = leitor.readLine()) != null) {
             System.out.println(linha);
         }
-
         leitor.close();
-        System.out.println(linha);
+
+        int cont = 0;
+
+        for (char c : linha.toCharArray()) {
+            if (c == 'A')
+                cont++;
+            if (cont>1)
+                System.out.println("maze must have exactly one start point");
+                cont = 0;
+                return;
+        }
+        //validate start and goal
+        for (char c : linha.toCharArray()) {
+            if (c == 'B')
+                cont++;
+            if (cont>1)
+                System.out.println("maze must have exactly one goal");
+                cont = 0;
+                return;
+        }
+        //Determine height and width of maze
+        String[] content = linha.split("\n");
+
+        height = content.length;
+        width = content[0].length();
+
+        //Keep track of walls
+
+        for (int i = 0; i < height; i++) {
+
+        }
     }
 }
